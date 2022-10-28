d8bab8e7295be7e14457c5e1e8d972bd0bf12262
lukasz-glaner <lukasz.glaner@gmail.com>
2022-10-28 12:04:50 +0200

    registration implementation and access to h2 db by ignoring csrf for h2-console

--- src/main/java/com/example/movieclub/config/security/CustomSecurityConfig.java
+++ src/main/java/com/example/movieclub/config/security/CustomSecurityConfig.java
@@ -2,9 +2,13 @@ package com.example.movieclub.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -13,13 +17,23 @@ class CustomSecurityConfig {
     private static final String USER_ROLE = "USER";
     private static final String EDITOR_ROLE = "EDITOR";
     private static final String ADMIN_ROLE = "ADMIN";
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authz) -> authz
                         .mvcMatchers("/admin/**").hasAnyRole(EDITOR_ROLE, ADMIN_ROLE)
                         .anyRequest().permitAll()
                 )
-                .formLogin(withDefaults());
+                .formLogin(login -> login
+                        .loginPage("/login").permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout/**", HttpMethod.GET.name()))
+                        .logoutSuccessUrl("/login?logout").permitAll()
+                );
+        http.csrf().ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+        http.headers().frameOptions().sameOrigin();
+
         return http.build();
     }
 
@@ -30,5 +44,10 @@ class CustomSecurityConfig {
                 "/scripts/**",
                 "/styles/**"
         );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
